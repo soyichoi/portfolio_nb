fb7b935af8ed5e498329e4c9fdd50a9040e3cd9f
soyichoi <thekiss1226@gmail.com>
2026-04-13 18:45:13 +0900

    0413

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,46 @@ const heroSlide = new Swiper(heroBnr,{
         },
     },
 })
+
+//베스트셀러
+const best = document.querySelectorAll('.best_wrap .swiper');
+const best_title = document.querySelectorAll('.best_wrap .tab_left a');
+console.log(best, best_title);
+
+//베스트셀러 swiper
+const topSwiper = new Swiper(best[0], {
+    slidesPerView: 5,
+})
+const bottomSwiper = new Swiper(best[1], {
+    slidesPerView: 5,
+})
+const outerSwiper = new Swiper(best[2], {
+    slidesPerView: 5,
+})
+
+//베스트셀러 탭메뉴
+//초기 상태 (첫번째만 보이기)
+best.forEach(function(el, i){
+    if (i == 0) {
+        el.classList.add('active');
+    }
+    else {
+        el.classList.remove('active');
+    }
+})
+//탭 클릭
+best_title.forEach(function(tab, index) {
+    tab.addEventListener('click', function(e) {
+        e.preventDefault();
+        //탭 active 처리
+        best_title.forEach(function(t){
+            t.classList.remove('active');
+        });
+        tab.classList.add('active');
+        //swiper 보여주기
+        best.forEach(function(swiper) {
+            swiper.classList.remove('active');
+        });
+        best[index].classList.add('active');
+    });
+});
