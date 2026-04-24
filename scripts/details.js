7d1379577a48bbaca860c04a57b5fa1bd1960958
soyichoi <thekiss1226@gmail.com>
2026-04-24 20:18:25 +0900

    0424 집가서 상세페이지 수정하기

--- scripts/details.js
+++ scripts/details.js
@@ -1,6 +1,15 @@
+//위로 script
+const fixedTopDetail = document.querySelector('.to_top_detail');
+//console.log(fixedTop);
+fixedTopDetail.style.display = 'none';
+window.addEventListener('scroll',function(){
+    if(this.window.scrollY >= 1000) fixedTopDetail.style.display = 'block';
+    else fixedTopDetail.style.display = 'none';
+})
+
 //상세페이지 상단 이미지 swiper
 const detailSwiper = document.querySelector('.detail_swiper');
-console.log(detailSwiper);
+//console.log(detailSwiper);
 
 const detailSlide = new Swiper(detailSwiper,{
     loop:true,
